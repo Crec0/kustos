f2dde3cd7302dd872aba15117fe61087364974d9
Crec0 <83436716+Crec0@users.noreply.github.com>
2024-01-05 20:38:30 -0800

    Abstract out error value wrapper

--- src/lib/server/utils/jwt.ts
+++ src/lib/server/utils/jwt.ts
@@ -1,60 +1,34 @@
 import { ISSUER } from '$env/static/private';
 import { publicKey } from '$lib/server';
+import { safeAwait, type Failure } from '$lib/server/utils/type-utils';
 import type { Cookies } from '@sveltejs/kit';
-import type { KeyObject } from 'crypto';
-import { jwtVerify, type JWTPayload, type JWTVerifyOptions, type JWTVerifyResult } from 'jose';
+import { jwtVerify, type JWTPayload } from 'jose';
 
 interface SessionJWT extends JWTPayload {
     userID: string;
 }
 
-type SafeVerifySuccess = {
-    success: true;
-    jwt: JWTVerifyResult<SessionJWT>;
-};
-
-type SafeVerifyError = {
-    success: false;
-    error: string;
-};
-
-type SafeVerifyResult = SafeVerifySuccess | SafeVerifyError;
-
-export async function safeVerifyJWT(
-    jwt: string,
-    key: KeyObject,
-    options?: JWTVerifyOptions | undefined,
-): Promise<SafeVerifyResult> {
-    try {
-        const verifiedJwt = await jwtVerify<SessionJWT>(jwt, key, options);
-        return { success: true, jwt: verifiedJwt };
-    } catch (e: unknown) {
-        if (e instanceof Error) {
-            return { success: false, error: e.message };
-        }
-        // Unknown error. It shouldn't reach here.
-        throw e;
-    }
-}
-
-export async function verifySession(cookies: string | undefined) {
-    if (cookies == null) {
+export async function verifySession(token: string | undefined) {
+    if (token == null) {
         return {
             success: false,
             error: 'Cookie not found',
-        } satisfies SafeVerifyError;
+        } satisfies Failure;
     }
-    return await safeVerifyJWT(cookies, publicKey, {
-        issuer: ISSUER,
-        clockTolerance: 30,
-    });
+
+    return await safeAwait(
+        jwtVerify<SessionJWT>(token, publicKey, {
+            issuer: ISSUER,
+            clockTolerance: 30,
+        }),
+    );
 }
 
 export async function verifyJWTAndGetUserID(cookies: Cookies) {
     const sessionTokenCookie = cookies.get('session_token');
-    const verificationResult = await verifySession(sessionTokenCookie);
-    if (verificationResult.success) {
-        return verificationResult.jwt.payload.userID;
+    const result = await verifySession(sessionTokenCookie);
+    if (result.success) {
+        return result.value.payload.userID;
     }
     return null;
 }
---
+++ src/lib/server/utils/type-utils.ts
@@ -0,0 +1,29 @@
+import { logger } from '$lib/server';
+
+export type Success<T> = {
+    success: true;
+    value: T;
+};
+
+export type Failure = {
+    success: false;
+    error: unknown;
+};
+
+export type Result<T> = Success<T> | Failure;
+
+export const safeAwait = async <T>(promise: Promise<T>): Promise<Result<T>> => {
+    try {
+        const value = await promise;
+        return { success: true, value: value };
+    } catch (e) {
+        if (e instanceof Error) {
+            return { success: false, error: e.message };
+        }
+        logger.error(e);
+        return {
+            success: false,
+            error: 'Unknown error occurred. Please check the logs for more information.',
+        };
+    }
+};
