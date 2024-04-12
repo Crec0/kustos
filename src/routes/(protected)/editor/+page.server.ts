0acff68b313ef29413ac785b5ff0551291fac3b2
Crec0 <83436716+Crec0@users.noreply.github.com>
2024-04-11 23:40:55 -0700

    Reattach server side handling

--- src/routes/(protected)/editor/+page.server.ts
+++ src/routes/(protected)/editor/+page.server.ts
@@ -1,7 +1,11 @@
 import { type Guild } from '$lib/schemas/discord-schema';
 import type { ParsedVersions } from '$lib/schemas/mc-versions-schema';
 import { postForm } from '$lib/schemas/post-form-schema';
-import { fail } from '@sveltejs/kit';
+import { logger } from '$lib/server';
+import { blobs, db, posts } from '$lib/server/database';
+import { upload } from '$lib/server/s3';
+import { generateUniqueID } from '$lib/server/utils/random';
+import { error, fail } from '@sveltejs/kit';
 import { zod } from 'sveltekit-superforms/adapters';
 import { superValidate, withFiles } from 'sveltekit-superforms/server';
 import type { PageServerLoad } from './$types';
@@ -23,13 +27,47 @@ export const load: PageServerLoad = async ({ fetch }) => {
 export const actions = {
     default: async ({ request }) => {
         const form = await superValidate(request, zod(postForm));
-
-        // TODO: Do stuff
-
         if (!form.valid) {
-            return fail(400, { form });
+            return fail(400, { form: withFiles(form) });
         }
+
+        const userID = request.headers.get('discord-user-id');
+        if (userID == null) {
+            logger.error("discord-user-id is null. This shouldn't happen.");
+            error(403, 'Invalid user id header');
+        }
+
+        const postID = generateUniqueID(posts, posts.id);
+
+        db.insert(posts)
+            .values({
+                id: postID,
+                name: form.data.name,
+                author: userID,
+                channel: form.data.channel,
+                credits: form.data.credits,
+                guild: form.data.guild,
+                tags: form.data.tag.join(','),
+                version: form.data.version,
+                description: form.data.description,
+            })
+            .returning()
+            .prepare()
+            .run();
+
+        await Promise.allSettled([
+            ...uploadAllOf(form.data.schematic, postID, 'schematic'),
+            ...uploadAllOf(form.data.image, postID, 'image'),
+        ]);
 
         return { form: withFiles(form) };
     },
 };
+
+const uploadAllOf = (formData: File[], postID: string, kind: string) =>
+    formData.map((f) => {
+        const id = generateUniqueID(blobs, blobs.id);
+        // TODO make this not so error prone
+        db.insert(blobs).values({ id: id, name: f.name, kind: kind, postId: postID }).returning().get();
+        return upload(`${kind}/${postID}/${id}`, f.arrayBuffer());
+    });
