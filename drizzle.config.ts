c929a909a88e3f5597f5f8965bd1223fa69ea886
Crec0 <83436716+Crec0@users.noreply.github.com>
2024-05-25 21:54:42 -0700

    Update libs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
     schema: './src/lib/server/database/schema.ts',
-    driver: 'better-sqlite',
+    dialect: 'sqlite',
     out: 'drizzle',
     dbCredentials: {
         url: './data.db',
