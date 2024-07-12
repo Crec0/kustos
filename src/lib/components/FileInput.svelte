bd6834199d175ba1eb144901e9e617f9f9312520
Crec0 <83436716+Crec0@users.noreply.github.com>
2024-07-12 10:55:10 -0700

    add drag/drop etc

--- src/lib/components/FileInput.svelte
+++ src/lib/components/FileInput.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    import { XIcon } from 'lucide-svelte';
+    import { Trash, Trash2, XIcon } from 'lucide-svelte';
     import { type Unsubscriber, type Updater, writable, type Writable } from 'svelte/store';
     import { Card, CardContent, CardHeader } from '$components/ui/card';
     import { Button } from '$components/ui/button';
+    import { cn } from '$utils';
 
     export let accept: string;
     export let body: string;
@@ -47,46 +48,100 @@
         event.currentTarget.value = '';
         update();
     };
+
+    function removeAnimationCleanly(target: HTMLLabelElement) {
+        const func = (pp: AnimationEvent) => {
+            if (pp && pp.target && pp.target instanceof HTMLLabelElement) {
+                pp.target.classList.remove('blinking');
+            }
+            target.removeEventListener('animationiteration', func);
+        };
+        target.addEventListener('animationiteration', func);
+    }
+
+    function dropHandler(
+        ev: DragEvent & { currentTarget: EventTarget & HTMLLabelElement; dataTransfer: DataTransfer | null },
+    ) {
+        ev.preventDefault();
+        removeAnimationCleanly(ev.currentTarget);
+
+        if (ev.dataTransfer == null) return;
+
+        const uploadedFiles = [];
+        for (let i = 0; i < ev.dataTransfer.files.length; i++) {
+            const file = ev.dataTransfer.files[i];
+            if ($filesArray.some((f) => f.name === file.name)) {
+                continue;
+            }
+            uploadedFiles.push(file);
+        }
+
+        $filesArray = [...$filesArray, ...uploadedFiles];
+        update();
+    }
+
+    function handleDragOver(ev: DragEvent & { currentTarget: EventTarget & HTMLLabelElement }) {
+        ev.preventDefault();
+
+        if (!ev.currentTarget.classList.contains('blinking')) {
+            ev.currentTarget.classList.add('blinking');
+        }
+    }
+
+    function handleDragEnd(ev: DragEvent & { currentTarget: EventTarget & HTMLLabelElement }) {
+        ev.preventDefault();
+
+        removeAnimationCleanly(ev.currentTarget);
+    }
 </script>
 
-<Card class="w-full max-w-lg p-2">
+<Card class={cn('w-full p-2', isImage ? '' : 'max-w-lg')}>
     <CardHeader
-        class={`flex ${$filesArray.length === 0 ? 'hidden' : ''} ${isImage ? 'flex-row flex-wrap' : 'flex-col'}`}
+        class={cn(
+            'flex space-y-0 p-2',
+            $filesArray.length === 0 ? 'hidden' : '',
+            isImage ? 'flex-row flex-wrap gap-2' : 'flex-col space-y-1.5',
+        )}
     >
         {#each $filesArray as file (file.name)}
-            <div class={`flex items-center space-x-2 rounded ${isImage ? 'relative w-max' : 'w-full'}`}>
-                {#if isImage}
-                    <img src={URL.createObjectURL(file)} alt={file.name} class="max-h-[160px]" />
-                    <button class="absolute right-2.5 top-2.5" on:click|preventDefault={() => removeFile(file)}>
-                        <XIcon
-                            class="variant-filled-surface hover:variant-filled-error rounded stroke-red-500 hover:stroke-red-100"
-                        />
-                    </button>
-                {:else}
-                    <div
-                        class="flex w-fit items-center gap-x-2 rounded transition-colors has-[#thing:hover]:bg-destructive/60 has-[#thing:hover]:text-destructive-foreground"
+            {#if isImage}
+                <div class="flex flex-col rounded bg-accent">
+                    <Button
+                        class="bg-secondary text-secondary-foreground"
+                        variant="destructive"
+                        id="thing"
+                        on:click={() => removeFile(file)}
                     >
-                        <span class="w-80 max-w-80 truncate px-2">
-                            {file.name}
-                        </span>
-                        <Button
-                            class="space-x-2 bg-secondary text-secondary-foreground"
-                            variant="destructive"
-                            id="thing"
-                            on:click={() => removeFile(file)}
-                        >
-                            <span> Remove </span>
-                            <XIcon />
-                        </Button>
-                    </div>
-                {/if}
-            </div>
+                        <Trash2 class="h-6 w-6" />
+                    </Button>
+                    <img src={URL.createObjectURL(file)} alt={file.name} class="h-[12rem] object-contain p-2" />
+                </div>
+            {:else}
+                <div
+                    class="flex w-full items-center gap-x-2 rounded transition-colors has-[.SELECTION:hover]:bg-destructive/20 has-[.SELECTION:hover]:text-destructive-foreground"
+                >
+                    <span class="grow truncate p-2">
+                        {file.name}
+                    </span>
+                    <Button
+                        class="SELECTION mr-2 bg-secondary text-secondary-foreground"
+                        variant="destructive"
+                        on:click={() => removeFile(file)}
+                    >
+                        <Trash2 class="h-6 w-6" />
+                    </Button>
+                </div>
+            {/if}
         {/each}
     </CardHeader>
     <CardContent class="p-2">
         <label
             class="hover:light:bg-accent/20 flex h-16 w-full cursor-pointer items-center justify-center rounded border-2 border-dashed transition-colors hover:border-accent hover:bg-accent/40 hover:text-foreground"
             for={name}
+            on:drop={dropHandler}
+            on:dragover={handleDragOver}
+            on:dragend={handleDragEnd}
+            on:dragleave={handleDragEnd}
         >
             {body}
         </label>
@@ -94,3 +149,36 @@
         <input {accept} class="hidden" id={name} multiple {name} on:change={updateFiles} type="file" />
     </CardContent>
 </Card>
+
+<style>
+    .blinking {
+        animation: blinking 1s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+    }
+
+    @keyframes blinking {
+        0%,
+        100% {
+            border-color: hsl(var(--accent));
+            scale: 1;
+            translate: 0 0;
+        }
+
+        25% {
+            border-color: hsl(var(--primary));
+            scale: 1.05;
+            translate: -10px 0;
+        }
+
+        50% {
+            border-color: hsl(var(--primary));
+            scale: 1.1;
+            translate: 0 0;
+        }
+
+        75% {
+            border-color: hsl(var(--primary));
+            scale: 1.05;
+            translate: 10px 0;
+        }
+    }
+</style>
