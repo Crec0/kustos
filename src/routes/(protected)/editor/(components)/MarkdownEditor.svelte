15e45a81ef9bf4c9942a45336c01f64051f7cc22
Crec0 <83436716+Crec0@users.noreply.github.com>
2024-09-16 17:14:43 -0700

    add markdown preview

--- src/routes/(protected)/editor/(components)/MarkdownEditor.svelte
+++ src/routes/(protected)/editor/(components)/MarkdownEditor.svelte
@@ -21,7 +21,10 @@
         closeBracketsKeymap,
         completionKeymap,
     } from '@codemirror/autocomplete';
-    import { onMount } from 'svelte';
+    import {
+        onMount,
+        tick,
+    } from 'svelte';
     import { EditorState } from '@codemirror/state';
     import { indentUnit } from '@codemirror/language';
     import {
@@ -58,32 +61,37 @@
     } from 'lucide-svelte';
     import { Separator } from '$components/ui/separator';
     import MKButton from './MKButton.svelte';
+    import { Switch } from '$components/ui/switch';
+    import { writable } from 'svelte/store';
+
+    import '../../../../github.css';
+    import { unified } from 'unified';
+    import remarkParse from 'remark-parse';
+    import remarkGfm from 'remark-gfm';
+    import remarkRehype from 'remark-rehype';
+    import rehypeStringify from 'rehype-stringify';
 
 
-    let value = `
-
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique tellus dui, nec sodales massa aliquam ac. Maecenas ac tempor mauris. Quisque odio sapien, accumsan nec orci vitae, placerat tincidunt mi. Nam non finibus augue. Nam nec condimentum velit, consequat venenatis massa. In hac habitasse platea dictumst. Aenean a aliquam lacus, at fermentum massa. Morbi venenatis neque id ante egestas varius. Nullam lectus nisl, hendrerit venenatis efficitur sed, commodo eu erat. Vestibulum id dictum massa, non gravida risus. Phasellus tincidunt fringilla urna, nec feugiat neque feugiat sed.
-
-Vestibulum quis sagittis neque. Donec dui nunc, scelerisque quis augue in, lacinia aliquam libero. Integer commodo purus fringilla ante sagittis egestas. Vestibulum eu leo eros. Ut hendrerit, dui eget pharetra tempus, ipsum nisl fringilla velit, quis bibendum turpis est sit amet nibh. Donec vulputate orci et risus ullamcorper, non placerat turpis fringilla. Ut molestie imperdiet elit eu auctor. Fusce suscipit ex id nulla scelerisque, in pellentesque dolor efficitur.
-
-Duis congue justo sed mi pharetra feugiat. Maecenas dictum aliquet metus sed aliquam. Nulla facilisi. Sed condimentum at tortor sed tristique. Ut ultrices ac mi eu elementum. Pellentesque cursus lacus sit amet sem ultrices blandit. Donec feugiat ex ante, eget iaculis lectus faucibus in. In non eros magna. Maecenas ac leo sed tellus sodales maximus.
-
-Cras feugiat odio a lectus cursus, sit amet varius mi molestie. Cras lacinia ipsum eget elit sagittis, pulvinar gravida ante efficitur. Maecenas feugiat ut erat nec interdum. In ut scelerisque leo. Nulla rhoncus, orci at condimentum aliquet, nulla lectus ultricies metus, id aliquam justo lacus sit amet nisl. In vel ultrices eros. Proin id urna tincidunt, aliquet risus accumsan, varius nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc ipsum urna, luctus at orci ac, dignissim rutrum metus. Vivamus quis ultrices tortor. Donec in sem sit amet augue placerat pulvinar non sed nunc. Nulla vel justo enim. Proin malesuada felis sit amet mi volutpat, at commodo turpis sollicitudin. Suspendisse tellus elit, imperdiet eu dictum id, tempus nec lectus.
-
-Vestibulum quis aliquam purus. Nullam mattis enim a nisi fringilla vehicula. Cras neque orci, congue in molestie nec, tristique sit amet ante. Donec laoreet accumsan turpis, vel blandit diam faucibus a. Curabitur euismod dictum massa sed dignissim. Integer congue eros vel erat blandit, sed iaculis enim iaculis. Nunc tempor tortor varius mauris maximus placerat. Nunc vitae condimentum tortor. Pellentesque luctus interdum eros id sollicitudin. Phasellus sapien sem, elementum sit amet est efficitur, scelerisque elementum leo.
-
-
-
-
-
-
-
-
-w`;
+    let value = '';
 
     let element: HTMLDivElement;
     let view: EditorViewType;
+    let previewElem: HTMLDivElement;
+    let preview = writable(false);
+
+    $: console.log($preview);
+
+    const remark = unified()
+        .use(remarkParse)
+        .use(remarkGfm)
+        .use(remarkRehype)
+        .use(rehypeStringify);
+
+    const parsePreview = async () => {
+        await tick();
+        const result = await remark.process(view.state.doc.toString());
+        previewElem.innerHTML = String(result);
+    };
 
     onMount(() => {
         view = new EditorView({
@@ -125,61 +133,67 @@ w`;
 <Card>
     <CardHeader class='p-2'>
         <div class='flex gap-2'>
-            <MKButton onClick={() => toggleWrapper(view, LUT.BOLD)} name='Bold'>
+            <MKButton name='Bold' onClick={() => toggleWrapper(view, LUT.BOLD)}>
                 <BoldIcon />
             </MKButton>
 
-            <MKButton onClick={() => toggleWrapper(view, LUT.ITALIC)} name='Italic'>
+            <MKButton name='Italic' onClick={() => toggleWrapper(view, LUT.ITALIC)}>
                 <ItalicIcon />
             </MKButton>
 
-            <MKButton onClick={() => toggleWrapper(view, LUT.UNDERLINE)} name='Underline'>
+            <MKButton name='Underline' onClick={() => toggleWrapper(view, LUT.UNDERLINE)}>
                 <UnderlineIcon />
             </MKButton>
 
-            <MKButton onClick={() => toggleWrapper(view, LUT.STRIKETHROUGH)} name='Strikethrough'>
+            <MKButton name='Strikethrough' onClick={() => toggleWrapper(view, LUT.STRIKETHROUGH)}>
                 <StrikethroughIcon />
             </MKButton>
 
-            <MKButton onClick={() => toggleWrapper(view, LUT.DETAIL_PREFIX, LUT.DETAIL_SUFFIX)} name='Spoiler'>
+            <MKButton name='Spoiler' onClick={() => toggleWrapper(view, LUT.DETAIL_PREFIX, LUT.DETAIL_SUFFIX)}>
                 <EyeOffIcon />
             </MKButton>
 
             <Separator orientation='vertical' />
 
-            <MKButton onClick={() => toggleLineBeginning(view, LUT.HEADING_1)} name='Heading 1'>
+            <MKButton name='Heading 1' onClick={() => toggleLineBeginning(view, LUT.HEADING_1)}>
                 <Heading1Icon />
             </MKButton>
 
-            <MKButton onClick={() => toggleLineBeginning(view, LUT.HEADING_2)} name='Heading 2'>
+            <MKButton name='Heading 2' onClick={() => toggleLineBeginning(view, LUT.HEADING_2)}>
                 <Heading2Icon />
             </MKButton>
 
-            <MKButton onClick={() => toggleLineBeginning(view, LUT.HEADING_3)} name='Heading 3'>
+            <MKButton name='Heading 3' onClick={() => toggleLineBeginning(view, LUT.HEADING_3)}>
                 <Heading3Icon />
             </MKButton>
 
             <Separator orientation='vertical' />
 
-            <MKButton onClick={() => toggleLineBeginning(view, LUT.ORDERED_LIST)} name='Ordered List'>
+            <MKButton name='Ordered List' onClick={() => toggleLineBeginning(view, LUT.ORDERED_LIST)}>
                 <ListOrderedIcon />
             </MKButton>
 
-            <MKButton onClick={() => toggleLineBeginning(view, LUT.UNORDERED_LIST)} name='Unordered List'>
+            <MKButton name='Unordered List' onClick={() => toggleLineBeginning(view, LUT.UNORDERED_LIST)}>
                 <ListIcon />
             </MKButton>
 
-            <MKButton onClick={() => toggleLineBeginning(view, LUT.TASK_LIST)} name='Task List'>
+            <MKButton name='Task List' onClick={() => toggleLineBeginning(view, LUT.TASK_LIST)}>
                 <ListTodoIcon />
             </MKButton>
 
-            <MKButton onClick={() => toggleLineBeginning(view, LUT.BLOCKQUOTE)} name='Blockquote'>
+            <MKButton name='Blockquote' onClick={() => toggleLineBeginning(view, LUT.BLOCKQUOTE)}>
                 <TextQuoteIcon />
             </MKButton>
+
+            <div class='flex space-x-2 items-center pr-2 ml-auto'>
+                <Switch bind:checked={$preview} id='preview-switch' on:click={parsePreview} />
+                <label for='preview-switch'>Preview</label>
+            </div>
         </div>
     </CardHeader>
     <CardContent class='p-0'>
         <Separator />
-        <div bind:this={element} class='codemirror-wrapper' />
+        <div bind:this={element} class={`codemirror-wrapper ${$preview ? "hidden" : ""}`} />
+        <div bind:this={previewElem} class={`markdown-body p-2 ${$preview ? "" : "hidden"}`}></div>
     </CardContent>
 </Card>
--- src/routes/(protected)/editor/+page.svelte
+++ src/routes/(protected)/editor/+page.svelte
@@ -60,19 +60,19 @@
         </CardContent>
     </Card>
 
-    <!--    <div class="relative mt-9">-->
-    <!--        <label class="text-md absolute -translate-y-7" for="archive-na`me"> Name </label>-->
-    <!--        <input-->
-    <!--            bind:value={$form.name}-->
-    <!--            class="variant-ringed-primary w-full rounded px-3 py-2"-->
-    <!--            id="name"-->
-    <!--            name="name"-->
-    <!--            type="text"-->
-    <!--        />-->
-    <!--    </div>-->
+        <div class="relative mt-9">
+            <label class="text-md absolute -translate-y-7" for="archive-na`me"> Name </label>
+            <input
+                bind:value={$form.name}
+                class="variant-ringed-primary w-full rounded px-3 py-2"
+                id="name"
+                name="name"
+                type="text"
+            />
+        </div>
 
-    <!--    <FileInput accept=".litematic" body="Add litematic(s)" files={schemProxy} name="schematic" />-->
-    <!--    <FileInput accept=".png,.jpg,.jpeg,.webp" body="Add image(s)" files={imgProxy} isImage name="image" />-->
+        <FileInput accept=".litematic" body="Add litematic(s)" files={schemProxy} name="schematic" />
+        <FileInput accept=".png,.jpg,.jpeg,.webp" body="Add image(s)" files={imgProxy} isImage name="image" />
 
     <Button type="submit">Submit</Button>
 </form>
